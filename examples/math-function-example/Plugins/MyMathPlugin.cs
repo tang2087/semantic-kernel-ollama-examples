215ac7180eca44ad217fc6584efbc36307155a88
Raymond Tang <fahao.tang@hotmail.com>
2024-05-14 21:27:21 +1000

    Added math plugin console .net project.

---
+++ examples/math-function-example/Plugins/MyMathPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+namespace math_function_example.Plugins;
+
+public sealed class MyMathPlugin
+{
+
+    [KernelFunction, Description("Add two integers")]
+    public static int Add(
+        [Description("The first integer to add")] int number1,
+        [Description("The second integer to add")] int number2
+    )
+    {
+        return number1 + number2;
+    }
+
+    [KernelFunction, Description("Subtract two integers")]
+    public static int Subtract(
+        [Description("The first integer to subtract from")] int number1,
+        [Description("The second integer to subtract away")] int number2
+    )
+    {
+        return number1 - number2;
+    }
+
+    [KernelFunction, Description("Multiply two integers.")]
+    public static int Multiply(
+        [Description("The first integer to multiply")] int number1,
+        [Description("The second integer to multiply")] int number2
+    )
+    {
+        return number1 * number2;
+    }
+
+    [KernelFunction, Description("Divide two integers. Make sure the second integer is not 0.")]
+    public static int Divide(
+        [Description("The first integer to multiply")] int number1,
+        [Description("The second integer to multiply")] int number2
+    )
+    {
+        return number1 / number2;
+    }
+    
+}
---
+++ examples/math-function-example/Program.cs
@@ -0,0 +1,78 @@
+﻿#pragma warning disable SKEXP0010
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.TemplateEngine;
+using math_function_example.Plugins;
+using static System.Console;
+
+
+// We use Mistral for this tutorial 
+var modelId = "mistral";
+// local Ollama endpoint
+var endpoint = new Uri("http://localhost:11434");
+
+var kernelBuilder = Kernel.CreateBuilder();
+kernelBuilder.Plugins.AddFromType<MyMathPlugin>();
+var kernel = kernelBuilder
+    .AddOpenAIChatCompletion(
+        modelId,
+        endpoint,
+        apiKey: null)
+    .Build();
+
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+var executionSettings = new OpenAIPromptExecutionSettings
+{
+    MaxTokens = 2000,
+    Temperature = 0,
+    // Enable automatic invocation of kernel functions
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+// Create chat history
+ChatHistory history = [];
+
+// Start the conversation
+Console.BackgroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.White; 
+Write("User > ");
+Console.ResetColor();
+string? userInput;
+while ((userInput = Console.ReadLine()) != null)
+{
+    history.AddUserMessage(userInput);
+    var result = chatService.GetStreamingChatMessageContentsAsync(
+                                history,
+                                executionSettings: executionSettings,
+                                kernel: kernel);
+
+    // Stream the results
+    string fullMessage = "";
+    var first = true;
+    await foreach (var content in result)
+    {
+        if (content.Role.HasValue && first)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White; 
+            Write("Assistant > ");
+            Console.ResetColor();
+            first = false;
+        }
+        Write(content.Content);
+        fullMessage += content.Content;
+    }
+
+    WriteLine();
+
+    // Add the message from the agent to the chat history
+    history.AddAssistantMessage(fullMessage);
+
+    // Get user input again
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.White; 
+    Write("User > ");
+    Console.ResetColor();
+}
